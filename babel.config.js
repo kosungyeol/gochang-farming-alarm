adfb71f57982851ff6138dc9e087474a8bcd923b
kosungyeol <54066561+kosungyeol@users.noreply.github.com>
2025-06-26 20:57:48 +0900

    ⚙️ Babel 설정 생성 - 코드 변환 및 경로 별칭

---
+++ babel.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ['@babel/plugin-transform-flow-strip-types'],
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@': './src',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@utils': './src/utils',
+          '@styles': './src/styles',
+          '@contexts': './src/contexts',
+          '@navigation': './src/navigation',
+          '@hooks': './src/hooks',
+          '@types': './src/types',
+        },
+      },
+    ],
+  ],
+};
